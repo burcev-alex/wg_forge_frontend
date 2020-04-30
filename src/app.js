f048637258bf987a21cfc5e4991bb46a05157a4d
Burtsev Alexander <Alexander.burzev@inbox.ru>
2020-05-01 00:45:21 +0300

    feat(app): формирование данных для рендеринга

--- src/app.js
+++ src/app.js
@@ -1,8 +1,108 @@
 // this is an example of improting data from JSON
-import 'orders' from '../data/orders.json';
+//import 'orders' from '../data/orders.json';
 
 export default (function () {
-    // YOUR CODE GOES HERE
-    // next line is for example only
-    document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
+    class Repository {
+        constructor() {
+            
+        }
+
+        async collections(){
+            let companies = await this._getCompanies();
+            let users = await this._getUsers();
+            let orders = await this._getOrders();
+            let data = {};
+
+            data.companies = companies;
+            data.users = users;
+            data.orders = orders;
+            
+            return data;
+        }
+
+        _getOrders() {
+            // запрашиваем JSON о заказах
+            return this._request('orders');
+        }
+
+        _getUsers(){
+            // запрашиваем JSON о пользователях
+            return this._request('users');
+        }
+
+        _getCompanies(){
+            // запрашиваем JSON о компаниях
+            return this._request('companies');
+        }
+
+        _request(type){
+            return fetch('/api/'+type+'.json').then(response => response.json());
+        }
+
+    }
+
+    class App {
+        constructor() {
+            this.data = [];
+        }
+
+        /**
+         * Инициализация
+         */
+        init() {
+            console.log('init');
+            let self = this;
+            this.getData().then((collections) => {
+                let items = [];
+
+                collections.orders.forEach(orderElement => {
+                    let user = {}
+
+                    collections.users.forEach(userElement => {
+                        if(userElement.id == orderElement.user_id){
+                            user = userElement;
+                        }
+                    });
+
+                    let company = {}
+
+                    collections.companies.forEach(companyElement => {
+                        if(companyElement.id == user.company_id){
+                            company = companyElement;
+                        }
+                    });
+
+                    orderElement.userInfo = user;
+                    orderElement.companyInfo = company;
+                    
+                    items.push(orderElement);
+                    
+                });
+                
+                self.data = items;
+                self.createTable();
+            });
+        }
+
+        /**
+         * Формирование данных
+         */
+        async getData() {
+            let repository = new Repository();
+            let collections = await repository.collections();
+
+            return collections;
+        }
+
+        /**
+         * Отрисовка таблицы
+         */
+        createTable(){
+            console.log(this.data);
+            document.getElementById("app").innerHTML = "<h1>Заказы</h1>";
+        }
+    }
+
+    const app = new App();
+    app.init();
 }());
